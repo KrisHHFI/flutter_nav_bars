ca06daa1e152f0afd7762e71cd573fd20d83f60e
KrisHHFI <kristopherpepperuk@gmail.com>
2024-10-25 16:08:15 +0300

    Minor styling tweaks 3

--- lib/widgets/nav_bar_five.dart
+++ lib/widgets/nav_bar_five.dart
@@ -86,7 +86,7 @@ class NavBarFiveState extends State<NavBarFive> {
               ),
               Container(
                 padding: EdgeInsets.fromLTRB(
-                    smallPadding, 0, smallPadding, smallPadding),
+                    smallPadding, smallPadding, smallPadding, smallPadding),
                 color: const Color.fromRGBO(0, 0, 0, 0.2),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
